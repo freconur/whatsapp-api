56788c488972db69e2ab61451526d499115ae9dc
Franco Condori <freconur@gmail.com>
2023-12-23 18:38:06 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const messageRouter = require('./routers/messageRouter')
+const whatsappClient = require('./services/WhatsappClient')
+whatsappClient.initialize()
+const whitelist = [
+  "http://localhost:3001", "http://localhost:3000"
+]
+const app = express()
+app.use(express.json())
+app.use(cors({origin:whitelist}))
+app.use(messageRouter)
+app.listen(3000, () => () => console.log(`Server is ready in on port ${process.env.PORT}`))
