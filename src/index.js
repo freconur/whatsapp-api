880177b59532fa1f379442b020e88cfb9d2616e3
Franco Condori <freconur@gmail.com>
2023-12-23 19:11:26 -0500

    cambio el port a un process env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-import { PORT } from './config'
 const messageRouter = require('./routers/messageRouter')
 const whatsappClient = require('./services/WhatsappClient')
 whatsappClient.initialize()
@@ -11,5 +10,5 @@ const app = express()
 app.use(express.json())
 app.use(cors({origin:whitelist}))
 app.use(messageRouter)
-app.listen(PORT)
+app.listen(process.env.PORT)
 // app.listen(3000, () => () => console.log(`Server is ready in on port ${process.env.PORT}`))
