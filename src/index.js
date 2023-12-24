986884309d78377fe81cd1a1e3258d97d8b81cee
Franco <freconur@gmail.com>
2023-12-24 17:45:55 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const puppeteer = require('puppeteer')
 const messageRouter = require('./routers/messageRouter')
 const whatsappClient = require('./services/WhatsappClient')
 whatsappClient.initialize()
 const whitelist = [
   "http://localhost:3001", "http://localhost:3000"
 ]
+
+// const browser = puppeteer.launch({headless: true, args: ['--no-sandbox']});
+// new Client({
+//   ...browser,
+//   puppeteer: {
+//       args: [
+//         '--no-sandbox',
+//         '--disable-setuid-sandbox'
+//       ],
+//       // authStrategy: // what ever authStrategy you are using
+//   },
+// })
 const app = express()
 app.use(express.json())
 app.use(cors({origin:whitelist}))
