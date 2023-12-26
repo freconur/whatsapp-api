ff5694458880d4f615b46be9381afa30d2ec852c
Franco <freconur@gmail.com>
2023-12-26 13:20:24 -0500

    dd

--- src/routers/messageRouter.js
+++ src/routers/messageRouter.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const router = new express.Router()
 const whatsappClient = require('../services/WhatsappClient')
 
 router.get('/',(req, res) => {
   res.send('holis')
 })
-router.post('/message', (req, res) => {
-  // res.header('Access-Control-Allow-Origin','*')
+router.post('/message',cors(), (req, res) => {
+  // res.header('Access-Control-Allow-Origin','https://attendance-system-blond.vercel.app')
   whatsappClient.sendMessage(req.body.phoneNumber, req.body.message)
   res.send()
 })
