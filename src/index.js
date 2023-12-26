153cc3c07819c658f9e38b63df021f2606750112
Franco <freconur@gmail.com>
2023-12-26 01:00:38 -0500

    agregando

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const messageRouter = require('./routers/messageRouter')
 const whatsappClient = require('./services/WhatsappClient')
 whatsappClient.initialize()
 const whitelist = [
-  "http://localhost:3001", "http://localhost:3000", "https://whatsapp-api-attendance.up.railway.app","http://whatsapp-api-attendance.up.railway.app"
+  "http://localhost:3001", "http://localhost:3000","http://whatsapp-api-attendance.up.railway.app"
 ]
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin:whitelist}))
 // app.use(cors({origin:whitelist}))
+app.use(cors({origin:whitelist}))
 app.use(messageRouter)
 app.listen(process.env.PORT || 3000)
 // app.listen(3000, () => () => console.log(`Server is ready in on port ${process.env.PORT}`))
